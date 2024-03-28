76697e01679a8be68b5bd90e14a60ab33fe5f437
Jasmine Decouzon <jaznycdev@gmail.com>
2024-03-28 12:38:24 -0400

    added comment

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.chat_models import ChatOpenAI
 
+# openai api key goes here
+#
 OPENAI_API_KEY = ""
 
 
